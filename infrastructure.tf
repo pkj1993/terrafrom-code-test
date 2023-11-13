ec931bfbc2ea44ac1c629f2ab069ff71e1f7c4e8
pkj1993 <pkj161293@gmail.com>
2023-11-14 00:36:26 +0530

    new terrafrom code

--- infrastructure.tf
+++ infrastructure.tf
@@ -209,3 +209,169 @@ resource "aws_secretsmanager_secret_version" "AWS_ACCESS_KEY_ID_version" {
   secret_id     = aws_secretsmanager_secret.AWS_ACCESS_KEY_ID.id
   secret_string = var.AWS_ACCESS_KEY_ID
 }
+
+# Task definition
+resource "aws_cloudwatch_log_group" "ecs-log-group" {
+  name = "/ecs/${var.PREFIX}-task-def"
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family                   = "${var.PREFIX}-task-def"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = "1024"
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "name": "ecs-runner",
+      "image": "747632300909.dkr.ecr.ap-northeast-1.amazonaws.com/ecs-runner:latest",
+      "cpu": 256,
+      "memory": 512,
+      "essential": true,
+      "network_mode": "awsvpc",
+      "portMappings": [
+        {
+            "containerPort": 80
+        }
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+            "awslogs-region" : "ap-northeast-1",
+            "awslogs-group" : "/ecs/${var.PREFIX}-task-def",
+            "awslogs-stream-prefix" : "ecs"
+        }
+      },
+      "command": ["./start.sh"],
+      "secrets": [{
+        "name": "PAT",
+        "valueFrom": "${aws_secretsmanager_secret.PAT.arn}"
+      },
+      {
+        "name": "ORG",
+        "valueFrom": "${aws_secretsmanager_secret.ORG.arn}"
+      },
+      {
+        "name": "REPO",
+        "valueFrom": "${aws_secretsmanager_secret.REPO.arn}"
+      },
+      {
+        "name": "AWS_DEFAULT_REGION",
+        "valueFrom": "${aws_secretsmanager_secret.AWS_DEFAULT_REGION.arn}"
+      },
+      {
+        "name": "AWS_SECRET_ACCESS_KEY",
+        "valueFrom": "${aws_secretsmanager_secret.AWS_SECRET_ACCESS_KEY.arn}"
+      },
+      {
+        "name": "AWS_ACCESS_KEY_ID",
+        "valueFrom": "${aws_secretsmanager_secret.AWS_ACCESS_KEY_ID.arn}"
+      }]
+    }
+  ]
+  TASK_DEFINITION
+}
+
+# A security group for ECS
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.PREFIX}-ecs-sg"
+  description = "Allow incoming traffic for ecs"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.PREFIX}_ecs_sg"
+  }
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "${var.PREFIX}-ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = "1"
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets          = [aws_subnet.private_subnet.id]
+    assign_public_ip = false
+  }
+}
+
+# Autoscaling
+resource "aws_appautoscaling_target" "dev_to_target" {
+  max_capacity = 2
+  min_capacity = 1
+  resource_id = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "dev_to_memory" {
+  name               = "dev-to-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dev_to_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.dev_to_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dev_to_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 80
+  }
+}
+
+resource "aws_appautoscaling_policy" "dev_to_cpu" {
+  name = "dev-to-cpu"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.dev_to_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.dev_to_target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.dev_to_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 60
+  }
+}
+
+# Giving a Fargate access to the Secrets in the Secret Manager
+resource "aws_iam_role_policy" "password_policy_secretsmanager" {
+  name = "password-policy-secretsmanager"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "secretsmanager:GetSecretValue"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
