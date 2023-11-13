30ee400eed1139baa564dacd1b9075e85a433a3c
pkj1993 <pkj161293@gmail.com>
2023-11-13 20:14:58 +0530

    new terrafrom code

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-variable "PAT" {}
-variable "ORG" {}
-variable "REPO" {}
-variable "AWS_DEFAULT_REGION" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_ACCESS_KEY_ID" {}
-
 variable "PREFIX" {
     default = "ecs-runner"
 }
@@ -14,12 +7,3 @@ variable "vpc_cidr" {
     default = "20.0.0.0/16"
 }
 
-variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
-    default = "20.0.0.0/24"
-}
-
-variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
-    default = "20.0.255.0/24"
-}
