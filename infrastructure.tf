07d232a9175c1c704084a3ac852e8aef4dccfb06
pkj1993 <pkj161293@gmail.com>
2023-11-13 20:45:01 +0530

    new terrafrom code

--- infrastructure.tf
+++ infrastructure.tf
@@ -122,3 +122,24 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     value = "enabled"
   }
 }
+
+# Task execution role for used for ECS
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.PREFIX}_ecs_task_execution_role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
