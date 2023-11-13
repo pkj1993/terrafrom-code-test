42672d8950a6ba5cc8af5d5bb7e4cbb3d3818147
pkj1993 <pkj161293@gmail.com>
2023-11-13 20:56:48 +0530

    new terrafrom code

--- variables.tf
+++ variables.tf
@@ -16,10 +16,3 @@ variable "public_subnet_cidr" {
     description = "CIDR for the Public Subnet"
     default = "20.0.255.0/24"
 }
-
-variable "PAT" {}
-variable "ORG" {}
-variable "REPO" {}
-variable "AWS_DEFAULT_REGION" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_ACCESS_KEY_ID" {}
