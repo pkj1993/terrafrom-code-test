5b55f4d1c07136c3f44c478a673f4710bd2cdf04
pkj1993 <pkj161293@gmail.com>
2023-11-14 13:37:14 +0530

    new terrafrom code

--- infrastructure.tf
+++ infrastructure.tf
@@ -316,8 +316,8 @@ resource "aws_ecs_service" "ecs_service" {
 
 # Autoscaling
 resource "aws_appautoscaling_target" "dev_to_target" {
-  max_capacity = 2
-  min_capacity = 1
+  max_capacity = 3
+  min_capacity = 2
   resource_id = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.ecs_service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace = "ecs"
